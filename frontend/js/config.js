c471268d6a645b8a848169d50edaddb8a1a38dc9
vortexvmms <vortexvmms@gmail.com>
2026-07-16 00:55:52 +0800

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,5 +3,5 @@
 // replace the text below with your backend address.
 // Example: BACKEND_URL: "https://vmms-backend.onrender.com"
 window.VMMS_CONFIG = {
-  BACKEND_URL: "https://https://vmms-backend.onrender.com"
+  BACKEND_URL: "https://https://vmms-backend-46x7.onrender.com"
 };
