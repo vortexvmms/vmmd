a60ae4b5f3024ae64d8238b0185e54cd02a4ead4
vortexvmms <vortexvmms@gmail.com>
2026-07-16 23:28:10 +0800

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // VMMS configuration
 window.VMMS_CONFIG = {
   BACKEND_URL: "https://vmms-backend-7j1v.onrender.com",
-  SUPABASE_URL: "PASTE-URL",
-  SUPABASE_PUBLISHABLE: "PASTE-KEY"
+  SUPABASE_URL: "https://lqnbdemtgkermhaqfboh.supabase.co",
+  SUPABASE_PUBLISHABLE: "sb_publishable_GV2oQS2wP2ltMeg2Teh1Rw_qT3lLGno"
 };
